cbc3d0dd84809e44e53d0c6c699a919466cb9c88
Azrara <azrara.9516@gmail.com>
2019-12-09 15:15:10 +0100

    Feed from Talos

--- feed.py
+++ feed.py
@@ -39,9 +39,19 @@ def malwareHash():
         for md5Mal in finalResult:
             fileResult.write("%s\n" % md5Mal)
 
- 
+def ipReputationTalos():
+    url = "https://talos-intelligence-site.s3.amazonaws.com/production/document_files/files/000/087/936/original/ip_filter.blf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXACIED2SPMSC7GA%2F20191209%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20191209T110649Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=7d358841d5307b2d9262fa9f8001e5042d8d090b218e0627240af477aaf1a71b"
+    req = requests.get(url)
+    result = req.content
+    resultDecoded = result.decode("utf-8")
+    finalResult = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", resultDecoded)
+
+    with open('ipReputationTalos.txt', 'w') as fileResult:
+        for urlHaus in finalResult:
+            fileResult.write("%s\n" % urlHaus)
 
 if __name__ == "__main__":
     urlhaus()
     c2servers()
     malwareHash()
+    ipReputationTalos()
