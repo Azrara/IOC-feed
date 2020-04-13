180ed564903f89c9e2bc673032956d45a82ccf63
Azrara <azrara.9516@gmail.com>
2020-04-13 19:09:34 +0200

    Feeds from Abuse.ch and Talos Intelligence.

--- feed.py
+++ feed.py
@@ -1,57 +1,66 @@
-
 import requests
 import csv
 import re
+from itertools import zip_longest
 
+
+def getConnection(urlSrc):
+    """get data from a specific url"""
+    url = urlSrc
+    req = requests.get(url)
+    result = req.content
+    resultDecoded = result.decode("utf-8")
+    return resultDecoded
 
 def urlhaus() :
-    url = "https://urlhaus.abuse.ch/downloads/csv_recent"
-    req = requests.get(url)
-    result = req.content
-    resultDecoded = result.decode("utf-8")
-    finalResult = re.findall('http?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', resultDecoded)
+    """get recent malicious url"""
+    result = getConnection("https://urlhaus.abuse.ch/downloads/csv_online/")
+    re1 = 'http?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+    re2 = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+    finaleResulNotCleaned = re.compile("(%s|%s)" % (re1,re2)).findall(result)
+        #Remove repeated URLs
+    finaleResulNotCleaned = list(set(finaleResulNotCleaned))
+    finaleResulCleaned = []
+    for line in finaleResulNotCleaned:
+        if line == "https://urlhaus.abuse.ch":
+            finaleResulNotCleaned.remove(line)
+        else:
+            if "http://" in line:
+                finaleResulCleaned.append(line.replace("http://",""))
+            elif "https://" in line :
+                finaleResulCleaned.append(line.replace("https://",""))
 
-    with open('urlhausMal.txt', 'w') as fileResult:
-        for urlHaus in finalResult:
-            fileResult.write("%s\n" % urlHaus)
-
+    return finaleResulCleaned
 
 def c2servers ():
-    url = "https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt"
-    req = requests.get(url)
-    result = req.content
-    resultDecoded = result.decode("utf-8")
-    finalResult = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", resultDecoded)
+    """get recent c2 servers"""
+    result = getConnection("https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt")
+    finalResult = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", result)
+    return finalResult
 
-    with open('c2servers.txt', 'w') as fileResult:
-        for ip in finalResult:
-            fileResult.write("%s\n" % ip)
-
-
-def malwareHash():
-    url="https://feodotracker.abuse.ch/downloads/malware_hashes.txt"
-    req = requests.get(url)
-    result = req.content
-    resultDecoded = result.decode("utf-8")
-    finalResult= re.findall(r"([a-fA-F\d]{32})", resultDecoded)
-
-    with open('md5Mal.txt', 'w') as fileResult:
-        for md5Mal in finalResult:
-            fileResult.write("%s\n" % md5Mal)
-
+""" def malwareHash():
+    """get hashes corresponding to recent malwares"""
+    result = getConnection("https://feodotracker.abuse.ch/downloads/malware_hashes.txt")
+    finalResult= re.findall(r"([a-fA-F\d]{32})", result)
+    return finalResult
+ """
+ 
 def ipReputationTalos():
-    url = "https://talos-intelligence-site.s3.amazonaws.com/production/document_files/files/000/087/936/original/ip_filter.blf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIXACIED2SPMSC7GA%2F20191209%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20191209T110649Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=7d358841d5307b2d9262fa9f8001e5042d8d090b218e0627240af477aaf1a71b"
-    req = requests.get(url)
-    result = req.content
-    resultDecoded = result.decode("utf-8")
-    finalResult = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", resultDecoded)
+    result = getConnection("http://www.talosintelligence.com/documents/ip-blacklist")
+    finalResult = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", result)
+    return finalResult
 
-    with open('ipReputationTalos.txt', 'w') as fileResult:
-        for urlHaus in finalResult:
-            fileResult.write("%s\n" % urlHaus)
 
 if __name__ == "__main__":
-    urlhaus()
-    c2servers()
-    malwareHash()
-    ipReputationTalos()
+    results = {}
+    urls = urlhaus()
+    ip = c2servers()
+    hashes = malwareHash()
+
+    results.update({"URLs":urls, "IP":ip, "Hashes":hashes})
+    transportedData = list(zip_longest(*results.values()))
+
+    with open("results.csv","w",newline="") as finalResult:
+        writer = csv.writer(finalResult)
+        writer.writerow(["URLs","IP","Hashes"])
+        writer.writerows(transportedData)   
